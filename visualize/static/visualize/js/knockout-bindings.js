ca5b328d3bc3ecbe0c17b088c21e531310eedc6d
Seth Hill <seth@pointnineseven.com>
2015-03-11 09:57:46 -0700

    MP-638: Read more

--- visualize/static/visualize/js/knockout-bindings.js
+++ visualize/static/visualize/js/knockout-bindings.js
@@ -72,13 +72,20 @@ ko.bindingHandlers.jqSlider = {
 
 ko.bindingHandlers.truncatedText = {
     update: function (element, valueAccessor, allBindingsAccessor) {
-        var originalText = ko.utils.unwrapObservable(valueAccessor()) || "",
-            // 30 is a default maximum length
-            length = ko.utils.unwrapObservable(allBindingsAccessor().maxTextLength) || 30,
-            truncatedText = originalText.length > length ? originalText.substring(0, length) + "..." : originalText;
+        var originalText = ko.utils.unwrapObservable(valueAccessor()) || "";
+        var length = ko.utils.unwrapObservable(allBindingsAccessor().maxTextLength) || 30;
+        var truncatedText;
+
+        if (originalText.length > length) {
+            truncatedText = originalText.substring(0, length) + "…";
+        }
+        else {
+            truncatedText = originalText;
+        }
+
         // updating text binding handler to show truncatedText
         ko.bindingHandlers.text.update(element, function () {
-            return truncatedText; 
+            return truncatedText;
         });
     }
 };
