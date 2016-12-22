7ca41fb1e4ad5c2f2934e516485aed0c04e15a0b
Ryan Hodges <ryan.d.hodges@gmail.com>
2016-12-21 18:10:12 -0800

    New drawings can be multipolygons

--- visualize/static/js/drawing.js
+++ visualize/static/js/drawing.js
@@ -1,14 +1,14 @@
 
 function drawingModel(options) {
     var self = this;
-    
+
     var ret = scenarioModel.apply(this, arguments);
-    
+
     //self.isSelectionModel = true;
-            
+
     //self.pointControl = new OpenLayers.Control.DrawFeature(pointLayer, OpenLayers.Handler.Point);
     //self.lineControl = new OpenLayers.Control.DrawFeature(lineLayer, OpenLayers.Handler.Path);
-    
+
     //will need to distinguish between drawing types...
     self.edit = function() {
         self.drawing = this;
@@ -17,10 +17,10 @@ function drawingModel(options) {
         }
         //app.viewModel.scenarios.drawingFormModel.polygonLayer.addFeatures([new OpenLayers.Feature.Vector(new OpenLayers.Geometry.fromWKT($('#id_geometry_orig')[0].value))]);
         //app.viewModel.scenarios.drawingFormModel.polygonLayer.addFeatures([new OpenLayers.Format.WKT().read($('#id_geometry_orig')[0].value)]);
-        
+
         //app.setLayerVisibility(drawing, false);
         return $.ajax({
-            url: '/features/aoi/' + self.drawing.uid + '/form/', 
+            url: '/features/aoi/' + self.drawing.uid + '/form/',
             success: function(data) {
                 app.viewModel.scenarios.drawingForm(true);
                 app.viewModel.scenarios.drawingFormModel = new polygonFormModel();
@@ -29,26 +29,26 @@ function drawingModel(options) {
                 app.viewModel.scenarios.drawingFormModel.originalDrawing = self.drawing;
                 app.viewModel.scenarios.drawingFormModel.polygonLayer = self.drawing.layer;
                 //debugger;
-                
+
                 app.map.zoomToExtent(self.drawing.layer.getDataExtent());
                 app.map.zoomOut();
-                
+
                 $('#drawing-form').html(data);
                 ko.applyBindings(app.viewModel.scenarios.drawingFormModel, document.getElementById('drawing-form'));
-                
+
                 app.viewModel.scenarios.drawingFormModel.showEdit(true);
                 app.viewModel.scenarios.drawingFormModel.hasShape(true);
                 app.viewModel.scenarios.drawingFormModel.startEdit();
             },
-            error: function (result) { 
-                //debugger; 
+            error: function (result) {
+                //debugger;
             }
         });
-    }; 
-        
+    };
+
     self.createCopy = function() {
         var drawing = this;
-    
+
         //create a copy of this shape to be owned by the user
         $.ajax({
             url: '/scenario/copy_design/' + drawing.uid + '/',
@@ -65,7 +65,7 @@ function drawingModel(options) {
     };
     self.deleteScenario = function() {
         var drawing = this;
-        
+
         //remove from activeLayers
         app.viewModel.activeLayers.remove(drawing);
         //remove from app.map
@@ -74,7 +74,7 @@ function drawingModel(options) {
         }
         //remove from selectionList
         app.viewModel.scenarios.drawingList.remove(drawing);
-        
+
         //remove from server-side db (this should provide error message to the user on fail)
         $.jsonrpc('delete_drawing', [drawing.uid], {
             //complete: drawingModel.loadDrawingList
@@ -103,67 +103,98 @@ function drawingModel(options) {
 
 function polygonFormModel(options) {
     var self = this;
-    
+
     self.newPolygonLayerName = "New Polygon Design 23";
-        
+
     self.isDrawing = ko.observable(false);
     self.showEdit = ko.observable(false);
     self.isEditing = ko.observable(false);
     self.hasShape = ko.observable(false);
-    
+
     self.polygonLayer = new OpenLayers.Layer.Vector(self.newPolygonLayerName);
     app.map.addLayer(self.polygonLayer);
-    
+
     self.polygonControl = new OpenLayers.Control.DrawFeature(self.polygonLayer, OpenLayers.Handler.Polygon);
     app.map.addControl(self.polygonControl);
-    
+
     self.editControl = new OpenLayers.Control.ModifyFeature(self.polygonLayer);
     app.map.addControl(self.editControl);
-    
+
     //self.selectFeature = new OpenLayers.Control.SelectFeature(self.polygonLayer);
     //app.map.addControl(self.selectFeature);
-    
+
     self.polygonControl.events.register(
-        'featureadded', 
-        self.polygonLayer, 
-        function(e) { 
-            self.completeSketch(); 
+        'featureadded',
+        self.polygonLayer,
+        function(e) {
+            self.completeSketch();
             self.showEdit(true);
         }
     );
-       
+
     self.startEdit = function() {
         self.isEditing(true);
-        //activate the modify feature control 
+        //activate the modify feature control
         self.editControl.activate();
         //disable feature attribution
         app.viewModel.disableFeatureAttribution();
         //select polygon
         self.editControl.selectFeature(self.polygonLayer.features[0]);
     };
-         
+
     self.completeEdit = function() {
         self.isEditing(false);
-        //deactivate the modify feature control 
+        //deactivate the modify feature control
         self.editControl.deactivate();
         //re-enable feature attribution
         app.viewModel.enableFeatureAttribution();
         //advance form (in case this was called clicking Done Editing button
         $('#button_next').click();
     };
-       
+
     self.completeSketch = function() {
         self.hasShape(true);
         self.isDrawing(false);
         //deactivate the draw feature control
         self.polygonControl.deactivate();
+
+
+        //Handle multipolygons
+        if (self.polygonLayer.features.length > 1) {
+          feature_list = self.polygonLayer.features;
+          polygon_list = [];
+          for (var i=0; i < feature_list.length; i++) {
+            if (feature_list[i].geometry instanceof OpenLayers.Geometry.Polygon) {
+              polygon_list.push(feature_list[i].geometry)
+            } else if (feature_list[i].geometry instanceof OpenLayers.Geometry.Multipolygon) {
+              for (var j=0; j < feature_list[i].geometry.components.length; j++) {
+                if (feature_list[i].geometry.components[j] instanceof OpenLayers.Geometry.Polygon) {
+                  polygon_list.push(feature_list[i].geometry.components[j])
+                }
+              }
+            } else {
+              window.alert('New geometry is not a polygon and will not be added to the drawing.');
+            }
+          }
+          multipolygon = new OpenLayers.Geometry.MultiPolygon;
+          for (var poly_idx = 0; poly_idx < polygon_list.length; poly_idx++) {
+            multipolygon.addComponent(polygon_list[poly_idx]);
+          }
+          self.polygonLayer.features[0].geometry = multipolygon;
+          while (self.polygonLayer.features.length > 1) {
+            self.polygonLayer.features.pop(1);
+          }
+        }
+
+
+
         //re-enable feature attribution
         app.viewModel.enableFeatureAttribution();
-    };   
+    };
 
     self.startSketch = function() {
         self.isDrawing(true);
-        //activate the draw feature control 
+        //activate the draw feature control
         self.polygonControl.activate();
         //disable feature attribution
         app.viewModel.disableFeatureAttribution();
@@ -187,6 +218,6 @@ function polygonFormModel(options) {
         //might make the name slightly more cryptic for this...
         app.map.removeLayerByName(self.newPolygonLayerName);
     };
-    
+
     return self;
 } // end polygonFormModel
