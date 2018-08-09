3b5fc6a7b721a8853c987bac4ba4389f387ccc80
Ryan Hodges <ryan.d.hodges@gmail.com>
2018-08-09 16:42:05 -0700

    helping building left-nav cope with wrong-site layer ghost records

--- visualize/static/js/server.js
+++ visualize/static/js/server.js
@@ -25,84 +25,90 @@ app.viewModel.loadLayers = function(data) {
                 // create a layerModel and add it to the list of layers
                 var layer = self.layerIndex[layer_id];
 
-                // Construct the text document to be searched
-                var searchText = layer.name + ' ' + 
-                        themeFixture.display_name + ' ' +
-                        themeFixture.description + ' ' +
-                        layer.description + ' ' + 
-                        layer.overview;
-                var searchKey = layer.name;
+                if (layer) {  // There is an issue with data-manager where sometimes layers that don't belong
+                                //    to this site are getting though
 
-                layer.themes.push(theme);
-                theme.layers.push(layer);
-                
-                if (!layer.subLayers.length) { //if the layer does not have sublayers
-                    self.layerSearchIndex[searchKey] = {
-                        layer: layer,
-                        searchText: searchText,
-                        theme: theme
-                    };
-                } else { 
-                    //if the layer has sublayers
-                    $.each(layer.subLayers, function(i, subLayer) {
-                        // Construct the text document to be searched
-                        var searchText = subLayer.name + ' ' + 
-                                themeFixture.display_name + ' / ' +
-                                themeFixture.description + ' ' +
-                                subLayer.parent.name + ' ' +
-                                subLayer.parent.overview + ' ' +
-                                subLayer.parent.description;
-                        var searchKey = subLayer.name; 
+                  // Construct the text document to be searched
+                  var searchText = layer.name + ' ' +
+                          themeFixture.display_name + ' ' +
+                          themeFixture.description + ' ' +
+                          layer.description + ' ' +
+                          layer.overview;
+                  var searchKey = layer.name;
 
-                        if (subLayer.name !== 'Data Under Development') {
-                            self.layerSearchIndex[searchKey] = {
-                                layer: subLayer,
-                                searchText: searchText,
-                                theme: theme
-                            };
-                        }
-                    });
-                    //sort by order id  
-                    layer.subLayers.sort( function(a,b) {
-                        if (a.order === b.order) {
-                            //sort alphabetically if id order is the same
-                            return a.name.toUpperCase().localeCompare(b.name.toUpperCase());
-                        } else {
-                            return a.order - b.order  
-                        } 
-                    });
-                } 
+                  layer.themes.push(theme);
+                  theme.layers.push(layer);
 
+                  if (!layer.subLayers.length) { //if the layer does not have sublayers
+                      self.layerSearchIndex[searchKey] = {
+                          layer: layer,
+                          searchText: searchText,
+                          theme: theme
+                      };
+                  } else {
+                      //if the layer has sublayers
+                      $.each(layer.subLayers, function(i, subLayer) {
+                          // Construct the text document to be searched
+                          var searchText = subLayer.name + ' ' +
+                                  themeFixture.display_name + ' / ' +
+                                  themeFixture.description + ' ' +
+                                  subLayer.parent.name + ' ' +
+                                  subLayer.parent.overview + ' ' +
+                                  subLayer.parent.description;
+                          var searchKey = subLayer.name;
+
+                          if (subLayer.name !== 'Data Under Development') {
+                              self.layerSearchIndex[searchKey] = {
+                                  layer: subLayer,
+                                  searchText: searchText,
+                                  theme: theme
+                              };
+                          }
+                      });
+                      //sort by order id
+                      layer.subLayers.sort( function(a,b) {
+                          if (a.order === b.order) {
+                              //sort alphabetically if id order is the same
+                              return a.name.toUpperCase().localeCompare(b.name.toUpperCase());
+                          } else {
+                              return a.order - b.order
+                          }
+                      });
+                  }
+                }
             });
             //sort by order id
-            theme.layers.sort( function(a,b) { 
+            theme.layers.sort( function(a,b) {
                 if (a.order === b.order) {
                     //sort alphabetically if id order is the same
                     return a.name.toUpperCase().localeCompare(b.name.toUpperCase());
                 } else {
                     return a.order - b.order
                 }
-            });  
+            });
 
-            
+
             self.themes.push(theme);
         } else {
             $.each(themeFixture.layers, function(j, layer_id) {
                 var layer = self.layerIndex[layer_id];
-                layer.themes.push(theme);
-                // if (layer.name === 'Canyon Labels' && $.browser.msie && $.browser.version < 9.0) {
-                //     //skip it
-                // } else {
-                     theme.layers.push(layer);
-                // }
+                if (layer) {    // There is an issue with data-manager where sometimes layers that don't belong
+                                //    to this site are getting though
+                  layer.themes.push(theme);
+                  // if (layer.name === 'Canyon Labels' && $.browser.msie && $.browser.version < 9.0) {
+                  //     //skip it
+                  // } else {
+                       theme.layers.push(layer);
+                  // }
+                }
             });
             //sort by order id
             theme.layers.sort( function(a,b) { return a.order - b.order });
-            
+
             self.hiddenThemes.push(theme);
         }
     });
-    
+
 	app.typeAheadSource = (function () {
             var items = [];
             for (var searchTerm in app.viewModel.layerSearchIndex) {
@@ -112,7 +118,7 @@ app.viewModel.loadLayers = function(data) {
             }
             return items;
     })();
-    
+
 };
 app.viewModel.loadLayersFromFixture = function() {
 	app.viewModel.loadLayers(app.fixture);
