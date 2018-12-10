0e893e81a11620ce1243c32038f4faaf33ec3579
Ryan Hodges <ryan.d.hodges@gmail.com>
2018-12-10 11:43:18 -0800

    increased IE support for sliders

--- visualize/static/js/models.js
+++ visualize/static/js/models.js
@@ -914,6 +914,28 @@ function layerModel(options, parent) {
     };
 
     self.toggleMultilayer = function(values) {
+      // IE Object.assign fix via Andres Ilich: https://stackoverflow.com/a/39021339
+      if (typeof Object.assign != 'function') {
+        Object.assign = function(target) {
+          'use strict';
+          if (target == null) {
+            throw new TypeError('Cannot convert undefined or null to object');
+          }
+
+          target = Object(target);
+          for (var index = 1; index < arguments.length; index++) {
+            var source = arguments[index];
+            if (source != null) {
+              for (var key in source) {
+                if (Object.prototype.hasOwnProperty.call(source, key)) {
+                  target[key] = source[key];
+                }
+              }
+            }
+          }
+          return target;
+        };
+      }
       multilayerObject = Object.assign({},self.associated_multilayers);
       for (var i = 0; i < values.length; i++) {
         multilayerObject = multilayerObject[values[i]];
