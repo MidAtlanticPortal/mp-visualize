3a189a6163395d51d6914aed30c2698fc2a0ba59
Ryan Hodges <ryan.d.hodges@gmail.com>
2019-07-09 15:33:24 -0700

    django 2 url patterns update

--- visualize/urls.py
+++ visualize/urls.py
@@ -1,11 +1,13 @@
-from django.conf.urls import patterns, url
+# from django.conf.urls import patterns, url
+from django.urls import re_path, include
 from .views import (show_embedded_map,
                    show_mafmc_map, show_mobile_map, show_planner)
 from django.views.generic.base import TemplateView
-urlpatterns = patterns('',
-    url(r'^experiment$', TemplateView.as_view(template_name='visualize/experiment.html')),
-    url(r'^map', show_embedded_map, name="embedded_map"),
-    url(r'^mafmc', show_mafmc_map, name="mafmc_map"),
-    url(r'^mobile', show_mobile_map, name="mobile_map"),
-    url(r'^$', show_planner, name="planner"),
-)
+urlpatterns = [
+    #'',
+    re_path(r'^experiment$', TemplateView.as_view(template_name='visualize/experiment.html')),
+    re_path(r'^map', show_embedded_map, name="embedded_map"),
+    re_path(r'^mafmc', show_mafmc_map, name="mafmc_map"),
+    re_path(r'^mobile', show_mobile_map, name="mobile_map"),
+    re_path(r'^$', show_planner, name="planner"),
+]
