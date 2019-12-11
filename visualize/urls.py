8e5d779b7a9771b32d9cddbc1d47785884906c65
Ryan Hodges <ryan.d.hodges@gmail.com>
2019-12-11 14:43:42 -0800

    bringing back vizualize rpc commands (bookmarks)

--- visualize/urls.py
+++ visualize/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import patterns, url
 from views import (show_embedded_map,
                    show_mafmc_map, show_mobile_map, show_planner)
 from django.views.generic.base import TemplateView
+from visualize import rpc
 urlpatterns = patterns('',
     url(r'^experiment$', TemplateView.as_view(template_name='visualize/experiment.html')),
     url(r'^map', show_embedded_map, name="embedded_map"),
