bedf2821e5f4699b8912ae7fdde5169c245c8776
Ryan Hodges <ryan.d.hodges@gmail.com>
2021-08-25 18:08:15 -0700

    enabling bookmark summary to be reached via django rest

--- visualize/models.py
+++ visualize/models.py
@@ -1,8 +1,13 @@
 from django.contrib.gis.db import models
 from features.registry import register
 from features.models import Feature
+from data_manager.models import Layer
+from drawing.models import AOI
 from tinymce.models import HTMLField
 from django.contrib.postgres.fields import JSONField
+from django.urls import reverse
+from django.contrib.contenttypes.models import ContentType
+import json
 
 @register
 class Bookmark(Feature):
@@ -13,6 +18,65 @@ class Bookmark(Feature):
     class Options:
         verbose_name = 'MARCO Bookmark'
         form = 'visualize.forms.BookmarkForm'
+
+    @property
+    def overview(self):
+
+        details = json.loads(self.json)
+        layers = []
+        for layer in details['layers']:
+            layer_dict = {
+                'name': layer['name']
+            }
+            if type(layer['id']) == int:
+                layer_obj = Layer.all_objects.get(pk=layer['id'])
+                content_type = ContentType.objects.get_for_model(layer_obj.__class__)
+                layer_dict['url'] = reverse ("admin:{}_{}_change".format(content_type.app_label, content_type.model), args=(layer['id'],))
+                layer_dict['description'] = ''
+                layer_dict['owner'] = {'name': '', 'url': False}
+            elif type(layer['id']) == str and 'drawing_aoi_' in layer['id']:
+                aoi_id = int(layer['id'].split('drawing_aoi_')[1])
+                drawing_obj = AOI.objects.get(pk=aoi_id)
+                content_type = ContentType.objects.get_for_model(drawing_obj.__class__)
+                layer_dict['url'] = reverse ("admin:{}_{}_change".format(content_type.app_label, content_type.model), args=(aoi_id,))
+                layer_dict['description'] = drawing_obj.description
+                user_ct = ContentType.objects.get_for_model(drawing_obj.user.__class__)
+                user_url = reverse ("admin:{}_{}_change".format(user_ct.app_label, user_ct.model), args=(drawing_obj.user.pk,))
+                layer_dict['owner'] = {'name': str(drawing_obj.user), 'url': user_url}
+            elif type(layer['id']) == str and 'user_layer_' in layer['id']:
+                layer_dict['url'] = layer['url']
+                layer_dict['description'] = 'User added (id: {})'.format(layer['arcgislayers'])
+                user_ct = ContentType.objects.get_for_model(self.user.__class__)
+                user_url = reverse ("admin:{}_{}_change".format(user_ct.app_label, user_ct.model), args=(self.user.pk,))
+                layer_dict['owner'] = {'name': str(self.user), 'url': user_url}
+            elif type(layer['id']) == str and 'mdat_layer_' in layer['id']:
+                layer_dict['url'] = layer['url']
+                layer_dict['description'] = 'MDAT Layer (id: {})'.format(layer['arcgislayers'])
+                layer_dict['owner'] = {'name': '', 'url': False}
+            elif type(layer['id']) == str and 'vtr_layer_' in layer['id']:
+                layer_dict['url'] = layer['url']
+                layer_dict['description'] = 'VTR Layer (id: {})'.format(layer['arcgislayers'])
+                layer_dict['owner'] = {'name': '', 'url': False}
+            else:
+                layer_dict['url'] =  False
+                layer_dict['description'] = ''
+                layer_dict['owner'] = {'name': '', 'url': False}
+
+            layers.append(layer_dict)
+
+        bookmark_ct = ContentType.objects.get_for_model(self.__class__)
+        bookmark_url = reverse ("admin:{}_{}_change".format(bookmark_ct.app_label, bookmark_ct.model), args=(self.pk,))
+        user_ct = ContentType.objects.get_for_model(self.user.__class__)
+        user_url = reverse ("admin:{}_{}_change".format(user_ct.app_label, user_ct.model), args=(self.user.pk,))
+        overview_dict = {
+            'url': '/visualize/#{}'.format(self.url_hash),
+            'name': self.name,
+            'admin': bookmark_url,
+            'description': self.description,
+            'owner': {'name': str(self.user), 'url': user_url},
+            'layers': layers
+        }
+        return overview_dict
 
 class Content(models.Model):
     name = models.CharField(max_length=100)
--- visualize/urls.py
+++ visualize/urls.py
@@ -5,6 +5,23 @@ except (ModuleNotFoundError, ImportError) as e:
 from .views import (show_embedded_map,
                    show_mafmc_map, show_mobile_map, show_planner, proxy_request)
 from django.views.generic.base import TemplateView
+
+from rest_framework import routers, serializers, viewsets
+from .models import Bookmark
+
+class BookmarkSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Bookmark
+        fields = ['overview',]
+
+class BookmarkViewSet(viewsets.ModelViewSet):
+    queryset = Bookmark.objects.all()
+    serializer_class = BookmarkSerializer
+
+router = routers.DefaultRouter()
+router.register(r'bookmarks', BookmarkViewSet)
+
+
 urlpatterns = [
     #'',
     re_path(r'^experiment$', TemplateView.as_view(template_name='visualize/experiment.html')),
@@ -12,5 +29,6 @@ urlpatterns = [
     re_path(r'^mafmc', show_mafmc_map, name="mafmc_map"),
     re_path(r'^mobile', show_mobile_map, name="mobile_map"),
     re_path(r'^proxy', proxy_request, name='proxy_request'),
+    re_path('^rest/', include(router.urls)),
     re_path(r'^$', show_planner, name="planner"),
 ]
